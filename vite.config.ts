282f81e66cfb0128dcf66196e7c6ccc0f377bdc0
he2e2 <dddd6368@naver.com>
2024-10-04 15:45:58 +0900

    fix: Fixed vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,70 +1,10 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    // VitePWA({
-    //   registerType: 'autoUpdate',
-    //   devOptions: {
-    //     enabled: true,
-    //   },
-    //   manifest: {
-    //     name: 'oho',
-    //     short_name: 'oho',
-    //     description: 'oho with PWA',
-    //     start_url: '/',
-    //     display: 'standalone',
-    //     theme_color: '#ffffff',
-    //     icons: [
-    //       {
-    //         src: '/icons/favicon-16x16.png',
-    //         sizes: '16x16',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: '/icons/favicon-32x32.png',
-    //         sizes: '32x32',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: '/icons/favicon-96x96.png',
-    //         sizes: '96x96',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: '/icons/favicon-196x196.png',
-    //         sizes: '196x196',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: '/icons/favicon-128.png',
-    //         sizes: '128x128',
-    //         type: 'image/png',
-    //       },
-    //     ],
-    //   },
-    //   workbox: {
-    //     runtimeCaching: [
-    //       {
-    //         urlPattern:
-    //           /^http:\/\/apis\.data\.go\.kr\/B551011\/KorService1\/.*$/,
-    //         handler: 'NetworkFirst',
-    //         options: {
-    //           cacheName: 'public-api-cache',
-    //           expiration: {
-    //             maxEntries: 50,
-    //             maxAgeSeconds: 60 * 60 * 24,
-    //           },
-    //         },
-    //       },
-    //     ],
-    //   },
-    // }),
-  ],
+  plugins: [react()],
   base: '/',
   server: {
     port: 3000,
