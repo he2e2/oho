22042c22c712939d24e502df6fc8bd710ea5eb1a
he2e2 <dddd6368@naver.com>
2024-10-06 17:24:43 +0900

    feat: Added Error page styling

--- src/pages/error-page.tsx
+++ src/pages/error-page.tsx
@@ -1,7 +1,43 @@
+import { CustomButton } from '@/components';
+import styled from '@emotion/styled';
+import { useNavigate } from 'react-router-dom';
+
 export function ErrorPage() {
+  const navigate = useNavigate();
+
   return (
-    <div className='mw'>
-      <p>404 ERROR</p>
-    </div>
+    <styles.wrapper className='mw'>
+      <h2>
+        404 ERROR
+        <br />
+        페이지가 존재하지 않습니다.
+      </h2>
+      <div
+        onClick={() => {
+          navigate(`/`);
+        }}
+      >
+        <CustomButton type='button' name='홈으로 돌아가기' />
+      </div>
+    </styles.wrapper>
   );
 }
+
+const styles = {
+  wrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+
+    h2 {
+      color: #000;
+      font-size: 2rem;
+      font-style: normal;
+      font-weight: 800;
+      line-height: 3rem;
+      text-align: center;
+    }
+  `,
+};
